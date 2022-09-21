df900bbb38574f73bb807191947c24f6ee4c0613
awadh <awadh.hassan@student.moringaschool.com>
2022-09-21 13:38:33 +0300

    assignment done

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,17 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Top 7 Must-Have Browser Extensions for Designers
+        Years ago, you would only see a web browser as just a piece of software on top of your operating system.
+      </p>
+      <img src={image} alt="I made this"></img>
+      
+
+    </div>
+  )
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,22 @@
+import react from "react";
 import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
-}
+  
+    // <div id="home">
+    //   <h1 style ="text-color: firebrick">Name
+    // </div>
+    const styleObj = {
+      color : "firebrick"
+    }
+    return <div id="home">Home
+      <h1 style={styleObj}>{name} is a Web Developer from {city}</h1>
+    </div>;
+  }
+  
+  
+
 
 export default Home;
